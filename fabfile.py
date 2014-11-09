f220dfc9e3be6ff19b4e84c1ca66105e37087b14
Stefan Wikner <stefan.wikner@gmail.com>
2014-11-09 19:26:31 +0100

    Fixed

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import local, lcd, run, cd, sudo, settings, env
 
-env.sudo_prefix = env.sudo_prefix.replace("sudo -S", "sudo -SE")
+env.sudo_prefix = "sudo -SE"
 
 
 def update_repository(commit_args, directory="."):
